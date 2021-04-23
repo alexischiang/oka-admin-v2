58e80f39b1bdddf36e1e108b840badfeebec8e85
jianghanming <jianghanming@jianghanming.local>
2021-04-23 19:17:23 +0800

    init

---
+++ oka-admin-v2/src/index.tsx
@@ -0,0 +1,14 @@
+import './index.css'
+
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+import Login from './pages/Login/Login'
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Login />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
